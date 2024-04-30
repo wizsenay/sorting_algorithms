0972e36b537d0bdb0c889c372e84d944dc40f8a3
wizsenay <tomasfrehiwot@gmail.com>
2024-04-30 12:09:49 -0700

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list -  sorts a doubly linked list of integers in ascending
+ *		 order using the Insertion sort algorithm
+ * @list: a pointer to the head pointer of the list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *new;
+	int step = 0, key, index_node;
+
+	temp = *list;
+	while (temp->next)
+	{
+		step++;
+		new = *list;
+		for (index_node = 0; index_node < step; index_node++)
+			new = new->next;
+		while (new && new->prev)
+		{
+			if (new->n < new->prev->n)
+			{
+				key = new->n;
+				new->n = new->prev->n;
+				new->prev->n = key;
+				print_list(*list);
+				new = new->prev;
+			}
+			else
+				new = NULL;
+		}
+		temp = temp->next;
+	}
+}
--- sort.h
+++ sort.h
@@ -10,7 +10,7 @@
  */
 typedef struct listint_s
 {
-	const int n;
+	int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
@@ -19,5 +19,8 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+
 
 #endif
